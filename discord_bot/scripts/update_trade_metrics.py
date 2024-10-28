fcd4c9e735ac0794356864cdd5c9a7f8e3bc6540
Dylan Zeller <dylanzeller98@gmail.com>
2024-10-28 14:06:17 -0400

    update

--- discord_bot/scripts/update_trade_metrics.py
+++ discord_bot/scripts/update_trade_metrics.py
@@ -26,19 +26,20 @@ def update_trade_metrics():
     trades = session.query(Trade).all()
 
     for trade in trades:
-        print(trade.size)
-        if str(trade.size).upper() == "MAX":
+        if str(trade.size).upper() == "MAX" or str(trade.current_size).upper() == "MAX":
+            trade.current_size = decimal_or_zero(6)
             trade.size = decimal_or_zero(6)
             transactions = session.query(Transaction).filter(Transaction.trade_id == trade.trade_id).all()
-            for t in open_transactions:
+            for t in transactions:
                 if str(t.size).upper() == "MAX":
                     t.size = decimal_or_zero(6)
-        elif "x" in str(trade.size):
+        elif "x" in str(trade.size) or "x" in str(trade.current_size):
             trade.size = decimal_or_zero(str(trade.size).replace("x", ""))
+            trade.current_size = decimal_or_zero(str(trade.current_size).replace("x", ""))
             transactions = session.query(Transaction).filter(Transaction.trade_id == trade.trade_id).all()
-            for t in open_transactions:
+            for t in transactions:
                 if "x" in str(t.size):
-                    t.size = decimal_or_zero(str(trade.size).replace("x", ""))
+                    t.size = decimal_or_zero(str(t.size).replace("x", ""))
     
     session.commit()
 
